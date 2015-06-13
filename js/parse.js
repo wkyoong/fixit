56ff74feaad769d3fc412c5ff32c358f7b3ced28
wkyoong <wkyoong@hotmail.com>
2015-06-14 00:09:06 +0800

    Update parse.js

--- js/parse.js
+++ js/parse.js
@@ -114,8 +114,10 @@ Parse.User.logIn(mail, pass, {
 } 
 
 function logoutfn(){
+    console.log("logging out..");
     Parse.User.logOut();
     decompiletabbar();
+    console.log("logged out!");
 }
 
 ///////////////////////////////////////////////////////
@@ -252,11 +254,13 @@ var ProfilePicOb = Parse.Object.extend("ProfilePic");
     if(imagedata != "") {
             var image_file=Parse.User.current().id+".jpg";
 			var parseFile = new Parse.File(image_file,{base64:imagedata});
+            var parseThumb= new Parse.File(image_file,{base64:thumbdata});
 			//console.log(parseFile);
 				parseFile.save().then(function() {
 					var profilepic = new ProfilePicOb();
 					profilepic.set("poster_id",Parse.User.current().id);
                     profilepic.set("profilepic",parseFile);
+                    //profilepic.set("thumbnail",parseThumb);
 					profilepic.save(null, {
 						success:function(ob) {
 							//$.mobile.changePage("#home");
@@ -296,39 +300,150 @@ function onProfilePhotoReset(){
     $("#largeImageProfile").attr("src","img/userholder.png");
 }
 
+
+function updateskill(){
+    ons.navigator.pushPage("profile_add_skill.html");
+    if(Parse.User.current().get("skill1")==1){cb1=true}else{cb1=false};
+    if(Parse.User.current().get("skill2")==1){cb2=true}else{cb2=false};
+    if(Parse.User.current().get("skill3")==1){cb3=true}else{cb3=false};
+    if(Parse.User.current().get("skill4")==1){cb4=true}else{cb4=false};
+    if(Parse.User.current().get("skill5")==1){cb5=true}else{cb5=false};
+    if(Parse.User.current().get("skill6")==1){cb6=true}else{cb6=false};
+    if(Parse.User.current().get("skill7")==1){cb7=true}else{cb7=false};
+    if(Parse.User.current().get("skill8")==1){cb8=true}else{cb8=false};
+    
+    setTimeout(function()
+            { 
+                $("#checkbox1").attr("checked",cb1);
+                $("#checkbox2").attr("checked",cb2);
+                $("#checkbox3").attr("checked",cb3);
+                $("#checkbox4").attr("checked",cb4);
+                $("#checkbox5").attr("checked",cb5);
+                $("#checkbox6").attr("checked",cb6);
+                $("#checkbox7").attr("checked",cb7);
+                $("#checkbox8").attr("checked",cb8);
+                
+            }
+    , 250);
+}
+
+function updateskillbtn(){
+    
+    if(document.getElementById('checkbox1').checked){var sk1=1}else{var sk1=0};
+    if(document.getElementById('checkbox2').checked){var sk2=1}else{var sk2=0};
+    if(document.getElementById('checkbox3').checked){var sk3=1}else{var sk3=0};
+    if(document.getElementById('checkbox4').checked){var sk4=1}else{var sk4=0};
+    if(document.getElementById('checkbox5').checked){var sk5=1}else{var sk5=0};
+    if(document.getElementById('checkbox6').checked){var sk6=1}else{var sk6=0};
+    if(document.getElementById('checkbox7').checked){var sk7=1}else{var sk7=0};
+    if(document.getElementById('checkbox8').checked){var sk8=1}else{var sk8=0};
+    
+    var skills = Parse.User.current();
+
+    skills.set("skill1",sk1); 
+    skills.set("skill2",sk2);
+    skills.set("skill3",sk3); 
+    skills.set("skill4",sk4);
+    skills.set("skill5",sk5); 
+    skills.set("skill6",sk6);
+    skills.set("skill7",sk7); 
+    skills.set("skill8",sk8);
+    
+    skills.save(null, {success:function(ob) {}, error:function(e) {console.log("Oh crap", e);}})
+                        
+    ons.navigator.popPage();
+    
+}
+
+
+
 ///////////////////////////////////////////////////////
 /////////////////PROJECT MANAGEMENT////////////////////
 ///////////////////////////////////////////////////////
 
 function createNewProject(){
-    var projecttitle=$('#project_title').val();
-    var projectdesc=$('#project_desc').val();
-    var projectowner=Parse.User.current().id;
+    //var projecttitle=$('#project_title').val();
+    //var projectdesc=$('#project_desc').val();
+    //var projectowner=Parse.User.current().id;
     //console.log(projectowner);
     
     var ProjectObject = Parse.Object.extend("ProjectObject");
     var project = new ProjectObject();
     
-    project.set("userid",projectowner);
-    project.set("title",projecttitle);
-    project.set("desc",projectdesc);
+    project.set("userid",Parse.User.current().id);
+    project.set("title",$('#project_title').val());
+    project.set("desc",projectdesc=$('#project_desc').val());
+    ///////////////////////////
     
-    project.save(null, {
+    if(imagedata != "") {
+            var image_file=Parse.User.current().id+".jpg";
+    		var parseFile = new Parse.File(image_file,{base64:imagedata});
+            //var parseThumb= new Parse.File(image_file,{base64:thumbdata});
+			//console.log(parseFile);
+				parseFile.save().then(function() {
+					project.set("project_pic",parseFile);
+					project.save(null, {
+						success:function(ob) {
+							//$.mobile.changePage("#home");
+                            setTimeout(function(){
+                            // updateprofilepicafter();
+                            },1000);
+						}, error:function(e) {
+							console.log("Oh crap", e);
+						}
+					});
+					//cleanUp();
+				}, function(error) {
+					//console.log("Error");
+					console.log("Error with uploading pic: "+error);
+				});
+                
+		} 
+
+    
+    /////////////////////////
+ /*   project.save(null, {
       success: function(user) {
       //console.log("Project Created!")
     },
     error: function(user, error) {
     alert("Error: " + error.code + " " + error.message);
-  }})
+  }})*/
     ons.navigator.popPage(); 
     var projectquery = new Parse.Query(ProjectObject);
     projectquery.equalTo("userid",Parse.User.current().id);
     projectquery.descending("createdAt");
+    
+    setTimeout(function(){
     projectquery.first({
   success: function(object) {
     //   console.log(object.id);
     
-    populateproject(object.id);
+    // psuhing to project pic
+            var ProjectPicOb = Parse.Object.extend("ProjectPic");
+            var projectpicpush = new ProjectPicOb();
+            
+            parseFile.save().then(function() {
+                    projectpicpush.set("project_id",object.id);
+            		projectpicpush.set("projectpic",parseFile);
+					projectpicpush.save(null, {
+						success:function(ob) {
+							//$.mobile.changePage("#home");
+                            setTimeout(function(){
+                            // updateprofilepicafter();
+                            },1000);
+						}, error:function(e) {
+							console.log("Oh crap", e);
+						}
+					});
+					//cleanUp();
+				}, function(error) {
+					//console.log("Error");
+					console.log("Error with uploading pic: "+error);
+				});
+    
+    populateproject(object.id,1);
+    
     document.getElementById("my_project_listing").innerHTML = "";
     queryproject();
   },
@@ -336,7 +451,7 @@ function createNewProject(){
     alert("Error: " + error.code + " " + error.message);
   }
 });
-    
+    },3000); 
 }
 
 
@@ -344,30 +459,109 @@ function createNewProject(){
 var project_query=0;
 var project_current="";
 
-function populateproject(projectid){
+function populateproject(projectid,push){
 project_current=projectid;
-console.log(project_current);
-projectNavigator.pushPage('project_page.html');
-
+//console.log(project_current);
+if(push==1){projectNavigator.pushPage('project_page.html');}
 var projectquery = new Parse.Query(Parse.Object.extend("ProjectObject"));
+
 projectquery.get(projectid, {
   success: function(results) {
     //setTimeout(function() {
-    document.getElementById("project_page_title").innerHTML = results.get('title');
-    document.getElementById("project_page_desc").innerHTML = results.get('desc');      
-    document.getElementById("project_page_budget").innerHTML = results.get('budget');      
-    document.getElementById("project_page_date").innerHTML = results.get('date');      
-    document.getElementById("project_page_time").innerHTML = results.get('time');      
-    document.getElementById("project_page_service").innerHTML = results.get('service');      
-    document.getElementById("project_page_location").innerHTML = results.get('location');      
+    if(results.get('desc')==undefined){var descfield="Please describe the work"}/*else if(results.get('desc').length==0){var descfield="Please describe the work"}*/else{var descfield=results.get('desc')}
+    if(results.get('budget')==undefined){var budgetfield="How much is your budget?"}/*else if(results.get('budget').length==0){var budgetfield="How much is your budget?"}*/else{var budgetfield=results.get('budget')}
+    if(results.get('date')==undefined){var datefield="When do you need this service?"}/*else if(results.get('date').length==0){var datefield="When do you need this service?"}*/else{var datefield=results.get('date')}
+    if(results.get('time')==undefined){var timefield="When is the best time to contact you?"}/*else if(results.get('time').length==0){var timefield="When is the best time to contact you?"}*/else{var timefield=results.get('time')}
+    if(results.get('service')==undefined){var servicefield="What services do you need?"}/*else if(results.get('service').length==0){var servicefield="What services do you need?"}*/else{var servicefield=results.get('service')}
+    if(results.get('location')==undefined){var locationfield="Where would the work be?"}/*else if(results.get('location').length==0){var locationfield="Where would the work be?"}*/else{var locationfield=results.get('location')}
+    
+    document.getElementById("project_page_title").innerHTML = "<a class=\"black\" onclick='updateProjectText(\"text\",\"title\",\""+project_current+"\",\""+results.get('title')+"\")'>"+results.get('title')+"</a>";
+    document.getElementById("project_page_desc").innerHTML = "<a class=\"black\" onclick='updateProjectText(\"text\",\"desc\",\""+project_current+"\",\""+descfield+"\")'>"+descfield+"</a>";     
+    document.getElementById("project_page_budget").innerHTML = "<a class=\"black\" onclick='updateProjectText(\"budget\",\"budget\",\""+project_current+"\",\""+budgetfield+"\")'>"+budgetfield+"</a>";   
+    document.getElementById("project_page_date").innerHTML = "<a class=\"black\" onclick='updateProjectText(\"date\",\"date\",\""+project_current+"\",\""+datefield+"\")'>"+datefield+"</a>";      
+    document.getElementById("project_page_time").innerHTML = "<a class=\"black\" onclick='updateProjectText(\"time\",\"time\",\""+"\",\""+timefield+"\")'>"+timefield+"</a>"; 
+    document.getElementById("project_page_service").innerHTML = "<a class=\"black\" onclick='updateProjectText(\"service\",\"service\",\""+"\",\""+servicefield+"\")'>"+servicefield+"</a>";    
+    document.getElementById("project_page_location").innerHTML = "<a class=\"black\" onclick='updateProjectText(\"location\",\"location\",\""+"\",\""+locationfield+"\")'>"+locationfield+"</a>";     
     //}, 100);
-    // The object was retrieved successfully.
+    
+//    console.log(Math.floor($(window).width()/3));
+    getProjectImages();
   },
   error: function(object, error) {
     // The object was not retrieved successfully.
     // error is a Parse.Error with an error code and message.
   }
 });}
+
+function getProjectImages(){
+    var projectpicquery = new Parse.Query(Parse.Object.extend("ProjectPic"));
+    projectpicquery.equalTo("project_id",project_current);
+    projectpicquery.find({
+        
+    success:function(results) {
+    
+    var s = "";
+	for(var i=0; i<results.length; i++) {
+    
+    s+='<li class="projectimgli"><a onclick="fullscreenimg(\''+results[i].get("projectpic").url()+'\')"><img class="projectimg" src="'+results[i].get("projectpic").url()+'"</img></a></li>';
+    
+    }
+    document.getElementById("project_pics").innerHTML = s;
+    console.log(s);
+    setTimeout(function(){
+    $('.projectimg').css({"height":Math.floor($(window).width()/3)});
+    $('.projectimgli').css({"height":Math.floor($(window).width()/3)});
+    },1000);
+    },error: function(object,error){}
+
+});
+}
+
+function updateProjectText(type,field,projectid,placeholder){
+    //$(".project_list").hide();
+    //pfield=".project_"+field;
+    //$(".project_text").show();
+    console.log(placeholder);
+    console.log(field);
+    projectNavigator.pushPage("project_updater.html");
+        
+        setTimeout(function(){
+            if(field=="title"){$('#update_project_field').html('Project Title');}
+            if(field=="desc"){$('#update_project_field').html('Project Description');}
+            if(field=="budget"){$('#update_project_field').html('Project Budget');}
+            if(field=="date"){$('#update_project_field').html('Appointment Date');}
+            if(field=="time"){$('#update_project_field').html('Appointment Time');}
+            if(field=="location"){$('#update_project_field').html('Project Location');}
+            if(field=="service"){$('#update_project_field').html('Services Required');}
+            $('#update_project_text').val(placeholder);
+        }, 300);    
+    project_updating_field=field;
+}
+
+var project_updating_field="";
+
+function updateProjectField(){
+    var value =$('#update_project_text').val();
+    var Point = Parse.Object.extend("ProjectObject");
+    var point = new Point();
+    point.id = project_current;
+    point.set(project_updating_field, value);
+    point.save(null, {
+    success: function(point) {
+        ons.navigator.popPage();
+        populateproject(project_current,0);
+    // Saved successfully.
+    },
+    error: function(point, error) {
+    }
+    });
+}
+
+function projectpageback(){
+    ons.navigator.popPage();
+    queryproject();
+}
+
 
 function deleteProject(){
     var projectquery = new Parse.Query(Parse.Object.extend("ProjectObject"));
@@ -402,16 +596,7 @@ var projectquery = new Parse.Query(Parse.Object.extend("ProjectObject"));
     projectquery.limit(50);
     projectquery.equalTo("userid",Parse.User.current().id);
     projectquery.descending("createdAt");
-    
-    //Somehow the function is firing 3 times - probably due to onsen ui quirks -> catch and return if auto fire
-    
-    if(project_query>0){return;};
-    setTimeout(function() {
-    project_query=0;    
-    }, 1500);
-    project_query+=1;
-    console.log("Query project count: "+project_query);
-	
+ 	
     projectquery.find({
         
 		success:function(results) {
@@ -423,23 +608,79 @@ var projectquery = new Parse.Query(Parse.Object.extend("ProjectObject"));
                 var project_title= results[i].get("title");
                 var project_desc = results[i].get("desc");
                 var project_date = eval('returntime('+results[i].createdAt.getTime()+')');
+                var project_pic = results[i].get("project_pic").url();
+            //if(project_pic==undefined){project_pic="img/location1.png";}
                 
-            s+='<ons-list-item modifier="chevron" class="list-item-container" onclick="populateproject(\''+project_id+'\')"><ons-row>';
-            s+='<ons-col width="95px"><img src="img/location1.png" class="thumbnail"></img></ons-col>';
+            s+='<ons-list-item modifier="chevron" class="list-item-container" onclick="populateproject(\''+project_id+'\',1)"><ons-row>';
+            s+='<ons-col width="95px"><img src="'+project_pic+'" class="thumbnail"></img></ons-col>';
             s+='<ons-col><div class="name">'+project_title+'</div>';
-            s+='<div class="location"><i class="fa fa-clock-o"></i>'+project_date+'</div>';
+            s+='<div class="location"><i class="fa fa-clock-o"></i>'+' '+project_date+'</div>';
             s+='<div class="desc">'+project_desc+'</div></ons-col><ons-col width="40px"></ons-col></ons-row></ons-list-item>';
- 
+            
 			}
-            $("#my_project_listing").append(s);
+            $("#my_project_listing").html(s);
             //console.log(s);
             ons.compile(document.getElementById('project_div'));
+            
 		},error:function(e) {
 			//$.mobile.loading("hide");
  
 		}
 	});
 };
+
+
+var ProjectPicOb = Parse.Object.extend("ProjectPic");
+     
+    function saveProjectPic(){
+     
+    if(imagedata != "") {
+            var image_file=project_current+".jpg";
+    		var parseFile = new Parse.File(image_file,{base64:imagedata});
+            //var parseThumb= new Parse.File(image_file,{base64:thumbdata});
+			//console.log(parseFile);
+				parseFile.save().then(function() {
+					var projectpic = new ProjectPicOb();
+					projectpic.set("project_id",project_current);
+                    projectpic.set("projectpic",parseFile);
+                    //projectpic.set("thumbnail",parseThumb);
+					projectpic.save(null, {
+						success:function(ob) {
+							//$.mobile.changePage("#home");
+                            ons.navigator.popPage();
+                            setTimeout(function(){
+                             //do something
+                             },1000);
+						}, error:function(e) {
+							console.log("Oh crap", e);
+						}
+					});
+					cleanUp();
+				}, function(error) {
+					//console.log("Error");
+					console.log("Error with uploading pic: "+error);
+				});
+ 
+		} else {
+			var projectpic = new ProjectPicOb();
+            projectpic.set("project_id",project_current);
+			projectpic.save(null, {
+				success:function(ob) {
+					//$.mobile.changePage("#home");
+                    ons.navigator.popPage();
+				}, error:function(e) {
+					console.log("Oh crap", e);
+				}
+			});
+			cleanUp();
+ 
+		}
+        
+	};
+
+function onProjectPhotoReset(){
+    $("#largeImage").attr("src","");
+}
 
 function parse_country(id,country){
     var LocationObject = Parse.Object.extend("LocationObject");
